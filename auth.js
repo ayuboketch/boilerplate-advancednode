6ab69d623dd19b3b76594355fe00c318056b9416
Ayub Oketch <ayuboketch84@gmail.com>
2025-04-22 01:17:00 +0000

    project complete

--- auth.js
+++ auth.js
@@ -2,6 +2,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const bcrypt = require('bcrypt');
 const { ObjectID } = require('mongodb');
+const GitHubStrategy = require('passport-github').Strategy;
+// const clientID = process.env.GITHUB_CLIENT_ID;
+// const clientSecret = process.env.GITHUB_CLIENT_SECRET;
 
 module.exports = function (app, myDataBase) {
   passport.serializeUser((user, done) => {
@@ -24,4 +27,40 @@ module.exports = function (app, myDataBase) {
       return done(null, user);
     });
   }));
+  passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: 'https://ayuboketch.github.io/boilerplate-advancednode/auth/github/callback'
+  },
+    function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      //Database logic here with callback containing your user object
+      myDataBase.findOneAndUpdate(
+        { id: profile.id },
+        {
+          $setOnInsert: {
+            id: profile.id,
+            username: profile.username,
+            name: profile.displayName || 'John Doe',
+            photo: profile.photos[0].value || '',
+            email: Array.isArray(profile.emails)
+              ? profile.emails[0].value
+              : 'No public email',
+            created_on: new Date(),
+            provider: profile.provider || ''
+          },
+          $set: {
+            last_login: new Date()
+          },
+          $inc: {
+            login_count: 1
+          }
+        },
+        { upsert: true, new: true },
+        (err, doc) => {
+          return cb(null, doc.value);
+        }
+      );
+    }
+  ));
 }
--- routes.js
+++ routes.js
@@ -7,7 +7,8 @@ module.exports = function (app, myDataBase) {
       title: 'Connected to Database',
       message: 'Please log in',
       showLogin: true,
-      showRegistration: true
+      showRegistration: true,
+      showSocialAuth: true
     });
   });
 
@@ -54,6 +55,12 @@ module.exports = function (app, myDataBase) {
       res.redirect('/profile');
     }
   );
+
+  app.route('/auth/github').get(passport.authenticate('github'));
+  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    req.session.user_id = req.user.id;
+    res.redirect("/chat");
+  })
 
   app.use((req, res, next) => {
     res.status(404)
